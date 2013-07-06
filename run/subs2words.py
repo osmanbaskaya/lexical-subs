d16cb6fb3c700da364791501554b08c558e7ba16
osmanbaskaya <obaskaya@ku.edu.tr>
2013-07-06 10:09:31 +0300

    substitutes to words done

--- run/subs2words.py
+++ run/subs2words.py
@@ -6,10 +6,10 @@ __author__ = "Osman Baskaya"
 import sys
 import gzip
 
-dataset = sys.argv[1]
+data = sys.argv[1]
 n = int(sys.argv[2])
 
-subs = gzip.open(dataset + '.sub.gz').readlines()
+subs = gzip.open(data).readlines()
 for sub in subs:
     sub = sub.split()
     print ','.join(sub[1:n*2+1:2])
